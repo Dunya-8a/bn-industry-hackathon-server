f0410b909df9faf0438642af210d8c54046aabf6
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-03-25 14:45:55 +0000

    Updated server to run with cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
 const inventoryRoutes = require("./routes/inventoryRoutes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/", inventoryRoutes);
 
 app.listen(PORT, () => {
